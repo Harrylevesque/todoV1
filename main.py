7ea7dc266044ca3080e034788181a2a0a0f01eeb
Harrylevesque <harrylevesque17@gmail.com>
2025-05-16 22:08:29 -0400

    Implement load and save functionality for todos with JSON storage, first working version

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import json, os
+import json
+import os
 
-FILENAME = todos.json
-prompt_main = input("Welcome to the Todo App! Press write add to continue...")
+FILENAME = 'todos.json'
+
+def load_todos():
+    if os.path.exists(FILENAME):
+        with open(FILENAME, 'r') as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    return []
+def save_todos(todos):
+    with open(FILENAME, 'w') as f:
+        json.dump(todos, f)
+
 def main():
+    todos = load_todos()
+    prompt_main = input("Welcome to the Todo App! Type add, remove, or list: ")
     if prompt_main == "add":
-        list = input("Enter the items to add: ")
-        with open('todos.json') as f:
-            json.dump(todos, f)
-    elseif prompt_main == "remove":
-        remove = input("Enter the items to remove: ")
+        item = input("Enter the item to add: ")
+        todos.append(item)
+        save_todos(todos)
+    elif prompt_main == "remove":
+        item = input("Enter the item to remove: ")
+        if item in todos:
+            todos.remove(item)
+            save_todos(todos)
     elif prompt_main == "list":
+        print("Your todos:", todos)
+    main()
 
+if __name__ == "__main__":
+    main()
