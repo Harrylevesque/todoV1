e1a591a1d11baf641e9042e44075f9cadd7f65d9
Harrylevesque <harrylevesque17@gmail.com>
2025-05-16 22:03:08 -0400

    Fix filename for todos and update main functionality to handle JSON storage

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import json, os
 
-FILENAME = todos
+FILENAME = todos.json
 prompt_main = input("Welcome to the Todo App! Press write add to continue...")
-list = []
 def main():
     if prompt_main == "add":
         list = input("Enter the items to add: ")
+        with open('todos.json') as f:
+            json.dump(todos, f)
     elseif prompt_main == "remove":
         remove = input("Enter the items to remove: ")
     elif prompt_main == "list":
-        prin
+
